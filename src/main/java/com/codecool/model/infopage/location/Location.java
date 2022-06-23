6e907a16cf707dc57ef69546aab32c53878945f9
AronBodzas <aron.bodzas42@gmail.com>
2022-06-23 12:19:59 +0200

    Feat: needed methods for location class

--- src/main/java/com/codecool/model/infopage/location/Location.java
+++ src/main/java/com/codecool/model/infopage/location/Location.java
@@ -1,11 +1,12 @@
 package com.codecool.model.infopage.location;
 
-import com.codecool.model.infopage.location.wildlife.*;
+import com.codecool.model.infopage.location.wildlife.Wildlife;
 
 import java.util.List;
 
 public class Location {
-
+	private int id;
+	private int continentId;
 	private String name;
 	private String description;
 	private List<Wildlife> wildlife;
@@ -14,7 +15,10 @@ public class Location {
 	private String climate;
 	private String crisis;
 
-	public Location(String name, String description, List<Wildlife> wildlife, String video, String sound, String climate, String crisis) {
+	public Location(int id, int continentId, String name, String description, List<Wildlife> wildlife,
+					String video, String sound, String climate, String crisis) {
+		this.id = id;
+		this.continentId = continentId;
 		this.name = name;
 		this.description = description;
 		this.wildlife = wildlife;
@@ -22,5 +26,13 @@ public class Location {
 		this.sound = sound;
 		this.climate = climate;
 		this.crisis = crisis;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public List<Wildlife> getWildlife() {
+		return wildlife;
 	}
 }

7fab9fcef3c9a63ea9861ed8b9eb6a24a5390547
AronBodzas <aron.bodzas42@gmail.com>
2022-06-23 12:20:17 +0200

    Feat: needed methods for continent class

---
+++ src/main/java/com/codecool/model/infopage/Continent.java
@@ -0,0 +1,30 @@
+package com.codecool.model.infopage;
+
+import com.codecool.model.infopage.location.Location;
+
+import java.util.List;
+
+public class Continent {
+
+	private int id;
+	private String name;
+	private List<Location> locations;
+
+	public Continent(int id, String name, List<Location> locations) {
+		this.id = id;
+		this.name = name;
+		this.locations = locations;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Location getLocationById(int id){
+		return locations.get(0);
+	}
+}

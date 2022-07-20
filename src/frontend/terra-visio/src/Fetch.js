17b119deb10f97d0f5d741a7438c19e93e79f37c
bby2k <bakbence.2k@gmail.com>
2022-07-20 09:01:17 +0200

    Feat: Fetching continent data component

--- src/frontend/terra-visio/src/Fetch.js
+++ src/frontend/terra-visio/src/Fetch.js
@@ -2,6 +2,7 @@ import React, {useEffect} from 'react';
 import axios from 'axios';
 
 export function ContinentList(){
+    //useState & useEffect
     const [continentData, setContinentData] = React.useState([])
     useEffect( () => {
         fetch('/api/allContinents',
@@ -10,13 +11,21 @@ export function ContinentList(){
             .then(res => res.json())
             .then(data => setContinentData(data))
     }, [])
+    
+    function removeInnerHtml(id){
+        document.getElementById(id).innerHTML = "";
+    }
+    const clickHandler = event => {
+        let str = event.target.dataset.name;
+        console.log(str)
+    }
 
     return (
-        <>{continentData.length===0?"No data yet.":continentData.map(data => {
-            const clickHandler = event => {
-            }
-                return <button key={data.id}>{data.name}</button>
-            }
-        )}</>
+        <><div className={"button-collection"} id={"btn-box"}>
+            {continentData.length===0?"No data yet.":continentData.map(data => {
+                    return <button key={data.id} data-name={data.name} onClick={clickHandler}>{data.name}</button>
+                }
+            )}
+        </div></>
     )
 }

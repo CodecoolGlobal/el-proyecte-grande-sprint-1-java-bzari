3fb40893a7679a52d9e297987b689a06bee78a96
bby2k <bakbence.2k@gmail.com>
2022-07-21 12:42:57 +0200

    Feat: Create Routes for the continents and locations

--- src/frontend/terra-visio/src/App.js
+++ src/frontend/terra-visio/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import {ContinentList} from "./Fetch";
+import {ContinentList} from "./ContinentList";
+import React from "react";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {LocationList} from "./LocationList";
+import {Test} from "./Test";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <ContinentList/>
+          <Routes>
+              <Route path="/" element={<ContinentList/>}/>
+              <Route path="/:name/locationlist" element={<LocationList/>}/>
+              <Route path="/location/:name" element={<Test/>}/>
+          </Routes>
       </header>
     </div>
   );

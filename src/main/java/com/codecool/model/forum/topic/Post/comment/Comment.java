eaf31d42405b24acc5f7eb4033801b8501b570d1
AronBodzas <aron.bodzas42@gmail.com>
2022-08-01 14:06:57 +0200

    Feat: Comment entity

--- src/main/java/com/codecool/model/forum/topic/Post/comment/Comment.java
+++ src/main/java/com/codecool/model/forum/topic/Post/comment/Comment.java
@@ -1,14 +1,33 @@
 package com.codecool.model.forum.topic.Post.comment;
 
-import com.codecool.model.user.User;
+import com.codecool.model.forum.topic.Post.Post;
+import com.codecool.model.user.Users;
+import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.io.PipedOutputStream;
 import java.time.LocalDate;
 
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment {
 
-	private User user;
+	@Id
+	@GeneratedValue
+	private Long id;
 	private LocalDate timestamp;
 	private String message;
 	private int upvote;
+	@ManyToOne
+	private Users user;
+	@ManyToOne
+	private Post post;
 
 }

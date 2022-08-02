a7ec65d5f4ab43e21e9c2fc8d30322c7e04a4aa8
bby2k <bakbence.2k@gmail.com>
2022-08-02 09:47:08 +0200

    Docs: Basic data.sql for setting up data at the start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO continent (name) VALUES ('Europe');
+INSERT INTO location (climate, crisis, description, name, sound, video, continent_id) VALUES ('asd','asd', 'asd', 'Balaton', null, null, 1);

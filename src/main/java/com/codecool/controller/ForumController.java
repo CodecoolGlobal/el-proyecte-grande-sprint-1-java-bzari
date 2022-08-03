024b58137d29587b1f464232d9a3c8d23aaec08a
AronBodzas <aron.bodzas42@gmail.com>
2022-08-03 09:28:58 +0200

    Feat: Forum controller class

---
+++ src/main/java/com/codecool/controller/ForumController.java
@@ -0,0 +1,76 @@
+package com.codecool.controller;
+
+import com.codecool.model.forum.topic.Post.Post;
+import com.codecool.model.forum.topic.Post.comment.Comment;
+import com.codecool.model.forum.topic.Topic;
+import com.codecool.service.implementation.forum.PostService;
+import com.codecool.service.implementation.forum.TopicService;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@Controller
+@RequestMapping(value = "/forum")
+@Builder
+@AllArgsConstructor
+public class ForumController {
+
+    @Autowired
+    private TopicService topicService;
+    @Autowired
+    private PostService postService;
+
+    @GetMapping(value = "/topics")
+    @ResponseBody
+    public List<Topic> getAllTopics(){
+        return topicService.getAllTopics();
+    }
+    @PostMapping(value = "/topics/new")
+    public String addTopic(@RequestBody Topic topic){
+        topicService.addNewTopic(topic);
+        return "index";
+//        TODO : return to topics page
+    }
+
+
+    @GetMapping(value = "{topicId}")
+    @ResponseBody
+    public Topic getTopicById(@PathVariable Long topicId){
+        return topicService.getTopicById(topicId);
+    }
+
+    //    TODO : edit topic
+
+
+    @GetMapping(value = "posts/{postId}")
+    @ResponseBody
+    public Post getPostById(@PathVariable Long postId){
+        return postService.getPostById(postId);
+    }
+
+    //    TODO : edit post
+    @PostMapping(value = "/{topicId}/newPost")
+    public String addPost(@RequestBody Post post, @PathVariable Long topicId){
+        post.setTopic(topicService.getTopicById(topicId));
+        postService.addPost(post);
+    return "index";
+    }
+
+    @PostMapping(value = "/{topicId}/{postId}/newComment")
+    public void addCommentToPost(@RequestBody Comment comment, @PathVariable Long topicId, @PathVariable Long postId){
+
+    }
+    //    TODO : edit comment
+
+
+    @PostMapping(value = "/comments/{commentId}/upVote")
+    public void upVoteComment(@PathVariable Long commentId){
+
+    }
+
+}

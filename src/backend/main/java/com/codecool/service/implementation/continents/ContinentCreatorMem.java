444fc9acac8d25e08a1cc88c8c4a730209efd21c
bby2k <bakbence.2k@gmail.com>
2022-08-03 20:04:58 +0200

    Refactor: Move file to Backend repository

--- src/backend/main/java/com/codecool/service/implementation/continents/ContinentCreatorMem.java
+++ src/backend/main/java/com/codecool/service/implementation/continents/ContinentCreatorMem.java
@@ -3,7 +3,6 @@ package com.codecool.service.implementation.continents;
 import com.codecool.model.infopage.Continent;
 import com.codecool.service.dao.ContinentCreatorDao;
 import com.codecool.service.dao.LocationStorageDao;
-import com.codecool.service.implementation.continents.locations.LocationStorageMem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -24,11 +23,11 @@ public class ContinentCreatorMem implements ContinentCreatorDao {
     @Override
     public List<Continent> createAllContinent(){
         List<Continent> allContinent = new ArrayList<>();
-        allContinent.add(new Continent(1, "Australia_and_Oceania", locationStorage.getLocationsByContinentId(1)));
-        allContinent.add(new Continent(2, "Europe", locationStorage.getLocationsByContinentId(2)));
-        allContinent.add(new Continent(3, "North_America", locationStorage.getLocationsByContinentId(3)));
-        allContinent.add(new Continent(4, "South_America", locationStorage.getLocationsByContinentId(4)));
-        allContinent.add(new Continent(5, "Central_America", locationStorage.getLocationsByContinentId(5)));
+//        allContinent.add(new Continent(1, "Australia_and_Oceania", locationStorage.getLocationsByContinentId(1)));
+//        allContinent.add(new Continent(2, "Europe", locationStorage.getLocationsByContinentId(2)));
+//        allContinent.add(new Continent(3, "North_America", locationStorage.getLocationsByContinentId(3)));
+//        allContinent.add(new Continent(4, "South_America", locationStorage.getLocationsByContinentId(4)));
+//        allContinent.add(new Continent(5, "Central_America", locationStorage.getLocationsByContinentId(5)));
         return allContinent;
     }
 

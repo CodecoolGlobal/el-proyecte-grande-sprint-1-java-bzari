5bcfc654ff2a4540cc01719d2d10c6506f3fb7d2
bzari <brigitta.zari@gmail.com>
2022-08-03 10:08:14 +0200

    Init topics page

---
+++ src/frontend/terra-visio/src/ForumTopics.js
@@ -0,0 +1,12 @@
+import {Col, Container, ListGroup, Row} from "react-bootstrap";
+
+
+function Topics(){
+    return(
+        <Container>
+
+        </Container>
+    )
+}
+
+export default Topics;

728a3013ce45113a62191bc2194b7f608f196950
AronBodzas <aron.bodzas42@gmail.com>
2022-08-03 12:34:45 +0200

    Feat: get post by topic id in post repository

--- src/main/java/com/codecool/service/repositories/PostRepository.java
+++ src/main/java/com/codecool/service/repositories/PostRepository.java
@@ -3,7 +3,10 @@ package com.codecool.service.repositories;
 import com.codecool.model.forum.topic.Post.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     Post getPostById(Long id);
+    List<Post> getPostsByTopicId(Long topicId);
 }

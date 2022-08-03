0dee8b8ef2641a2c1985f7a4be8256a7c5280daa
AronBodzas <aron.bodzas42@gmail.com>
2022-08-03 12:37:02 +0200

    Feat: forum topics listed using fetch

--- src/frontend/terra-visio/src/ForumTopics.js
+++ src/frontend/terra-visio/src/ForumTopics.js
@@ -1,32 +1,33 @@
 import {Container, ListGroup} from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
+import {useParams} from "react-router-dom";
+import React, {useEffect} from 'react';
 
 
 function Topics(){
+    const { name } = useParams();
+    console.log(name)
+    const [forumTopicsData, setForumTopicsData] = React.useState([])
+    useEffect(() => {
+        fetch(`/forum/topics`,
+            {method:"GET",
+                headers:{"accept":"application/json"}})
+            .then(res => res.json())
+            .then(data => setForumTopicsData(data))
+    })
+
     return(
         <Container>
             <Card>
             <Card.Title>Forum topics</Card.Title>
             <ListGroup>
-
-                <ListGroup.Item action href="#link1">
+                {forumTopicsData.length===0?"No location-data accessible":forumTopicsData.map(data => {
+                return <ListGroup.Item action href={`/forum/topics/${data.id}/${data.title}`}>
                     <div className="ms-2 me-auto">
-                        <div className="font-weight-bold">"Topic title"</div>
-                        Last updated: "time"
+                        <div className="font-weight-bold">{data.title}</div>
+                        Last updated: {data.timestamp}
                     </div>
-                </ListGroup.Item>
-                <ListGroup.Item action href="#link2">
-                    <div className="ms-2 me-auto">
-                        <div className="font-weight-bold">"Topic title"</div>
-                        Last updated: "time"
-                    </div>
-                </ListGroup.Item>
-                <ListGroup.Item action href="#link3">
-                    <div className="ms-2 me-auto">
-                        <div className="font-weight-bold">"Topic title"</div>
-                        Last updated: "time"
-                    </div>
-                </ListGroup.Item>
+                </ListGroup.Item>})}
             </ListGroup>
             </Card>
         </Container>

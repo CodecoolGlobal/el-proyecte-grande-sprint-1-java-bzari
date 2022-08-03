7c955b606d202da0bc08bb2306ad328545df6b4d
AronBodzas <aron.bodzas42@gmail.com>
2022-08-03 12:37:30 +0200

    Feat: topic posts listed using fetch

--- src/frontend/terra-visio/src/TopicPosts.js
+++ src/frontend/terra-visio/src/TopicPosts.js
@@ -1,32 +1,34 @@
 import {Container, ListGroup} from "react-bootstrap";
 import Card from 'react-bootstrap/Card';
+import {useParams} from "react-router-dom";
+import React, {useEffect} from "react";
 
 
 function Posts(){
+    const { title, id } = useParams();
+    // console.log(id)
+    const [topicPostsData, setTopicPostsData] = React.useState([])
+    useEffect(() => {
+        fetch(`/forum/topics/${id}/posts`,
+            {method:"GET",
+                headers:{"accept":"application/json"}})
+            .then(res => res.json())
+            .then(data => setTopicPostsData(data))
+    })
+
     return(
         <Container>
             <Card>
-                <Card.Title>"Topic title"</Card.Title>
+                    <Card.Title>{title}</Card.Title>
                 <ListGroup>
+                    {topicPostsData.length===0?"No location-data accessible":topicPostsData.map(data => {
+                    return <ListGroup.Item action href="#link1">
+                        <div className="ms-2 me-auto">
+                            <div className="font-weight-bold">{data.title}</div>
+                            Uploaded by: {data.user.username} at {data.timestamp}
+                        </div>
+                    </ListGroup.Item>})}
 
-                    <ListGroup.Item action href="#link1">
-                        <div className="ms-2 me-auto">
-                            <div className="font-weight-bold">"Post title"</div>
-                            Uploaded by: "user" at "time"
-                        </div>
-                    </ListGroup.Item>
-                    <ListGroup.Item action href="#link2">
-                        <div className="ms-2 me-auto">
-                            <div className="font-weight-bold">"Post title"</div>
-                            Uploaded by: "user" at "time"
-                        </div>
-                    </ListGroup.Item>
-                    <ListGroup.Item action href="#link3">
-                        <div className="ms-2 me-auto">
-                            <div className="font-weight-bold">"Post title"</div>
-                            Uploaded by: "user" at "time"
-                        </div>
-                    </ListGroup.Item>
                 </ListGroup>
             </Card>
         </Container>

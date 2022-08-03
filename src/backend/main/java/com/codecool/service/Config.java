87d86566094e5ca7e5a9cd6248d704ac29add992
bby2k <bakbence.2k@gmail.com>
2022-08-03 20:00:22 +0200

    Refactor: Remove dead code

--- src/backend/main/java/com/codecool/service/Config.java
+++ src/backend/main/java/com/codecool/service/Config.java
@@ -1,16 +1,10 @@
 package com.codecool.service;
 
 import com.codecool.service.dao.*;
-import com.codecool.service.implementation.continents.ContinentCreatorMem;
-import com.codecool.service.implementation.continents.ContinentStorageMem;
-import com.codecool.service.implementation.continents.locations.LocationCreatorMem;
-import com.codecool.service.implementation.continents.locations.LocationStorageMem;
 import com.codecool.service.implementation.continents.locations.wildlife.WildLifeCreatorMem;
-import com.codecool.service.implementation.continents.locations.wildlife.WildLifeStorageMem;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
 

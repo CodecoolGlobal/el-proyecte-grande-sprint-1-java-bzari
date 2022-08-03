42d3eb9d4774f063bb59fe9aee2896991bd264f1
bzari <brigitta.zari@gmail.com>
2022-08-03 10:20:00 +0200

    Add basic code structure to topics page

--- src/frontend/terra-visio/src/ForumTopics.js
+++ src/frontend/terra-visio/src/ForumTopics.js
@@ -1,10 +1,34 @@
-import {Col, Container, ListGroup, Row} from "react-bootstrap";
+import {Container, ListGroup} from "react-bootstrap";
+import Card from 'react-bootstrap/Card';
 
 
 function Topics(){
     return(
         <Container>
+            <Card>
+            <Card.Title>Forum topics</Card.Title>
+            <ListGroup>
 
+                <ListGroup.Item action href="#link1">
+                    <div className="ms-2 me-auto">
+                        <div className="font-weight-bold">"Topic title"</div>
+                        Last updated: "time"
+                    </div>
+                </ListGroup.Item>
+                <ListGroup.Item action href="#link2">
+                    <div className="ms-2 me-auto">
+                        <div className="font-weight-bold">"Topic title"</div>
+                        Last updated: "time"
+                    </div>
+                </ListGroup.Item>
+                <ListGroup.Item action href="#link3">
+                    <div className="ms-2 me-auto">
+                        <div className="font-weight-bold">"Topic title"</div>
+                        Last updated: "time"
+                    </div>
+                </ListGroup.Item>
+            </ListGroup>
+            </Card>
         </Container>
     )
 }

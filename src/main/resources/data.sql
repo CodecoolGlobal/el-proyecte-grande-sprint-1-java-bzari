ed3fac87bed3730b91dd8d8be8ee655d60b705cf
bby2k <bakbence.2k@gmail.com>
2022-08-04 09:42:49 +0200

    Refactor: Relocated files

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO continent (name) VALUES ('Europe');
-INSERT INTO location (climate, crisis, description, name, sound, video, continent_id) VALUES ('asd','asd', 'asd', 'Balaton', null, null, 1);

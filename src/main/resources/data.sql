5a24c9b22f704ee012755506eec478ecf3c8ac47
AronBodzas <aron.bodzas42@gmail.com>
2022-08-04 14:29:43 +0200

    Refactor: inserted date

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO users (id, email, password, username) VALUES (1, 'asd', 'asd', 'testUsername');
-INSERT INTO topic (description, title, user_id) VALUES ('asd', 'testTopic', 1);
-INSERT INTO post (content, reply_count, timestamp, title, view_count, topic_id, user_id) VALUES ('asd', 0, current_timestamp,'testPostTitle', 0, 1, 1);
-INSERT INTO comment (message, upvote, post_id, user_id) VALUES ('testComment', 0, 1, 1);
+INSERT INTO users (id, email, password, username) VALUES (1, 'asd@gmail.com', 'asd', 'IamGod666');
+INSERT INTO topic (description, title, user_id) VALUES ('General discussion', 'General discussion', 1);
+INSERT INTO post (content, reply_count, timestamp, title, view_count, topic_id, user_id) VALUES ('Are they dangerous?', 0, current_timestamp,'Rabbits', 0, 1, 1);
+INSERT INTO comment (message, upvote, post_id, user_id) VALUES ('they will eat you:)', 0, 1, 1);

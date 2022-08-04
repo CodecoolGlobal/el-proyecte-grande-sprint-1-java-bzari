aafde76e53b6c8dcf262dd05e9a7d363fe88a3ea
bzari <brigitta.zari@gmail.com>
2022-08-04 08:40:05 +0200

    Add "timestamp": new field to Post model and new column in database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (id, email, password, username) VALUES (1, 'asd', 'asd', 'testUsername');
 INSERT INTO topic (description, title, user_id) VALUES ('asd', 'testTopic', 1);
-INSERT INTO post (content, reply_count, title, view_count, topic_id, user_id) VALUES ('asd', 0, 'testPostTitle', 0, 1, 1);
+INSERT INTO post (content, reply_count, timestamp, title, view_count, topic_id, user_id) VALUES ('asd', 0, current_timestamp,'testPostTitle', 0, 1, 1);
 INSERT INTO comment (message, upvote, post_id, user_id) VALUES ('testComment', 0, 1, 1);

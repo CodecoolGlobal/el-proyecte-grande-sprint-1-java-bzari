0f888f236c4e33e238e8591e13166e55437f6f50
bby2k <bakbence.2k@gmail.com>
2022-08-04 10:48:51 +0200

    Refactor: Remove unused classes from project

--- src/backend/main/java/com/codecool/service/dao/LocationCreatorDao.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.service.dao;
-
-import com.codecool.model.infopage.location.Location;
-
-import java.util.List;
-
-public interface LocationCreatorDao {
-    List<Location> createAllLocations();
-}

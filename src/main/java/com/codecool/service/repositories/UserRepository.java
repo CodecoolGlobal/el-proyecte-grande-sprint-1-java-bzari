4a56b5573701fecea0442d5e4db62e9371bba40e
AronBodzas <aron.bodzas42@gmail.com>
2022-08-04 14:11:38 +0200

    Feat: getuser by id

--- src/main/java/com/codecool/service/repositories/UserRepository.java
+++ src/main/java/com/codecool/service/repositories/UserRepository.java
@@ -4,4 +4,6 @@ import com.codecool.model.user.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
+
+    Users getUsersById(Long userId);
 }

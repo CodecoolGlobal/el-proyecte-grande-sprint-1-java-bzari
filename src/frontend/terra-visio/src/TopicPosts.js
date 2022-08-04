b75065e25193dfa2c4e971be03232b54bab048f9
bzari <brigitta.zari@gmail.com>
2022-08-04 08:38:53 +0200

    Refactor: delete unused lines

--- src/frontend/terra-visio/src/TopicPosts.js
+++ src/frontend/terra-visio/src/TopicPosts.js
@@ -6,7 +6,6 @@ import React, {useEffect} from "react";
 
 function Posts(){
     const { title, id } = useParams();
-    // console.log(id)
     const [topicPostsData, setTopicPostsData] = React.useState([])
     useEffect(() => {
         fetch(`/forum/topics/${id}/posts`,
@@ -22,7 +21,7 @@ function Posts(){
                     <Card.Title>{title}</Card.Title>
                 <ListGroup>
                     {topicPostsData.length===0?"No location-data accessible":topicPostsData.map(data => {
-                    return <ListGroup.Item action href="#link1">
+                    return <ListGroup.Item action href={`/forum/topics/posts/${data.id}/${data.title}/`}>
                         <div className="ms-2 me-auto">
                             <div className="font-weight-bold">{data.title}</div>
                             Uploaded by: {data.user.username} at {data.timestamp}

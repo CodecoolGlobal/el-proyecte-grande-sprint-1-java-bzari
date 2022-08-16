6b56d29367149502a392ec49b527199ef35ba28e
AronBodzas <aron.bodzas42@gmail.com>
2022-08-16 10:46:47 +0200

    Feat: get permissions and authorities

--- src/backend/main/java/com/codecool/model/user/ApplicationUserType.java
+++ src/backend/main/java/com/codecool/model/user/ApplicationUserType.java
@@ -1,6 +1,34 @@
 package com.codecool.model.user;
 
+import com.codecool.security.ApplicationUserPermission;
+import com.google.common.collect.Sets;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.codecool.security.ApplicationUserPermission.POST_READ;
+import static com.codecool.security.ApplicationUserPermission.POST_WRITE;
+
+@Getter
 public enum ApplicationUserType {
-    USER,
-    ADMIN
+    USER(Sets.newHashSet(POST_READ)),
+    MODERATOR(Sets.newHashSet(POST_READ)),
+    ADMIN(Sets.newHashSet(POST_WRITE, POST_READ));
+
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserType(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+        Set<SimpleGrantedAuthority> permissions = getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+        permissions.add(new SimpleGrantedAuthority("ROLE" + this.name()));
+        return permissions;
+    }
 }

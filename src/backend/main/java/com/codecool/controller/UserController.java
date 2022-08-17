40ef0840921ddefc832d3f819e02a14f8df7dac4
bzari <brigitta.zari@gmail.com>
2022-08-17 14:29:02 +0200

    Add user controller for registration and login

---
+++ src/backend/main/java/com/codecool/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.codecool.controller;
+
+import com.codecool.model.DTO.ApplicationUserDTO;
+import com.codecool.model.user.ApplicationUser;
+import com.codecool.service.users.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Component
+@RequestMapping("/user")
+public class UserController {
+
+    private final UsersService userService;
+
+    @Autowired
+    public UserController(UsersService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping ("/registration")
+    public String newUserRegistration(@RequestBody ApplicationUser user) {
+        userService.addNewRegisteredUser(user);
+        return "index";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody ApplicationUserDTO user){
+//        megkapja a usert akkor kellene nekünk ezt átadni valahova
+        return "index";
+    }
+}

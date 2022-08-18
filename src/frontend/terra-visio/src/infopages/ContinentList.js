5e27fffed5dc5dfe72b7b8e8d5c8680a7f09b5e0
bby2k <bakbence.2k@gmail.com>
2022-08-18 11:07:22 +0200

    Chores: Pass through context via useContext hook

--- src/frontend/terra-visio/src/infopages/ContinentList.js
+++ src/frontend/terra-visio/src/infopages/ContinentList.js
@@ -13,10 +13,13 @@ export function ContinentList(){
             .then(data => setContinentData(data))
     }, [])
 
+    const setCoordinate = useContext(CoordinateContext)
     return (
         <><div className={"button-collection"} id={"btn-box"}>
             {continentData.length===0?"No data yet.":continentData.map(data => {
-                    return <Link to={`/continent/${data.name}`}><ButtonCreator context={data.name}/></Link>
+                return <Link to={`/continent/${data.name}`}>
+                    <button onClick={()=>{setCoordinate([15, 0]); eventListener(data.name)}}>{data.name}</button>
+                </Link>
                 }
             )}
         </div></>

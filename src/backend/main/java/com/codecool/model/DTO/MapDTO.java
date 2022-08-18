cbb18bd118dd8fe5a8a160f9d8593bf1edc0a85a
bby2k <bakbence.2k@gmail.com>
2022-08-18 16:18:51 +0200

    Feat: Add continentId & altitude & longitude fields to MapDTO class

--- src/backend/main/java/com/codecool/model/DTO/MapDTO.java
+++ src/backend/main/java/com/codecool/model/DTO/MapDTO.java
@@ -3,9 +3,22 @@ package com.codecool.model.DTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 public class MapDTO {
     String name;
     Long id;
+    Long continentId;
+    BigDecimal altitude;
+    BigDecimal longitude;
+
+    public MapDTO(String name, Long id, Long continentId, BigDecimal altitude, BigDecimal longitude) {
+        this.name = name;
+        this.id = id;
+        this.continentId = continentId;
+        this.altitude = altitude;
+        this.longitude = longitude;
+    }
 
     public MapDTO(String name, Long id) {
         this.name = name;

e155e49050df38f98bbf10033a586306f6e2a331
bzari <brigitta.zari@gmail.com>
2022-08-18 11:42:14 +0200

    Change map marker to custom

--- src/frontend/terra-visio/src/worldmap/MapChart.js
+++ src/frontend/terra-visio/src/worldmap/MapChart.js
@@ -47,32 +47,98 @@ const MapChart = ( props ) => {
 
                     {/*ASIA*/}
                     <Marker coordinates={[85, 35]}>
-                        <circle r={3} fill="#E42" className={"marker continent-marker"}/>
+                        {/*<circle r={3} fill="#E42" className={"marker continent-marker"}/>*/}
+                        <g className={"marker continent-marker"}
+                            fill="none"
+                            stroke="#7dc1cd"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            transform="translate(-12, -24)"
+                        >
+                            <circle cx="12" cy="10" r="3" />
+                            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                        </g>
                     </Marker>
 
                     {/*AFRICA*/}
                     <Marker coordinates={[20, 10]}>
-                        <circle r={3} fill="#E42" className={"marker continent-marker"}/>
+                        {/*<circle r={3} fill="#E42" className={"marker continent-marker"}/>*/}
+                        <g className={"marker continent-marker"}
+                           fill="none"
+                           stroke="#7dc1cd"
+                           strokeWidth="2"
+                           strokeLinecap="round"
+                           strokeLinejoin="round"
+                           transform="translate(-12, -24)"
+                        >
+                            <circle cx="12" cy="10" r="3" />
+                            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                        </g>
                     </Marker>
 
                     {/*EUROPE*/}
                     <Marker coordinates={[10, 50]}>
-                        <circle r={3} fill="#E42" className={"marker continent-marker"}/>
+                        {/*<circle r={3} fill="#E42" className={"marker continent-marker"}/>*/}
+                        <g className={"marker continent-marker"}
+                           fill="none"
+                           stroke="#7dc1cd"
+                           strokeWidth="2"
+                           strokeLinecap="round"
+                           strokeLinejoin="round"
+                           transform="translate(-12, -24)"
+                        >
+                            <circle cx="12" cy="10" r="3" />
+                            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                        </g>
                     </Marker>
 
                     {/*NORTH-AMERICA*/}
                     <Marker coordinates={[-100, 40]}>
-                        <circle r={3} fill="#E42" className={"marker continent-marker"}/>
+                        {/*<circle r={3} fill="#E42" className={"marker continent-marker"}/>*/}
+                        <g className={"marker continent-marker"}
+                           fill="none"
+                           stroke="#7dc1cd"
+                           strokeWidth="2"
+                           strokeLinecap="round"
+                           strokeLinejoin="round"
+                           transform="translate(-12, -24)"
+                        >
+                            <circle cx="12" cy="10" r="3" />
+                            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                        </g>
                     </Marker>
 
                     {/*SOUTH-AMERICA*/}
                     <Marker coordinates={[-420, 190]}>
-                        <circle r={3} fill="#E42" className={"marker continent-marker"}/>
+                        {/*<circle r={3} fill="#E42" className={"marker continent-marker"}/>*/}
+                        <g className={"marker continent-marker"}
+                           fill="none"
+                           stroke="#7dc1cd"
+                           strokeWidth="2"
+                           strokeLinecap="round"
+                           strokeLinejoin="round"
+                           transform="translate(-12, -24)"
+                        >
+                            <circle cx="12" cy="10" r="3" />
+                            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                        </g>
                     </Marker>
 
                     {/*AUSTRALIA-OCEANIA*/}
                     <Marker coordinates={[495, -25]}>
-                        <circle r={3} fill="#E42" className={"marker continent-marker"}/>
+                        {/*<circle r={3} fill="#E42" className={"marker continent-marker"}/>*/}
+                        <g className={"marker continent-marker"}
+                           fill="none"
+                           stroke="#7dc1cd"
+                           strokeWidth="2"
+                           strokeLinecap="round"
+                           strokeLinejoin="round"
+                           transform="translate(-12, -24)"
+                        >
+                            <circle cx="12" cy="10" r="3" />
+                            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                        </g>
                     </Marker>
 
                 </ZoomableGroup>

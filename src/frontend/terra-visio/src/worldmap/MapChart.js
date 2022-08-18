c3fa2f996dbe74e1e30b1a83bc627bf6ddbda7fa
bby2k <bakbence.2k@gmail.com>
2022-08-18 16:17:42 +0200

    Refactor: Remove unneeded className attribute

--- src/frontend/terra-visio/src/worldmap/MapChart.js
+++ src/frontend/terra-visio/src/worldmap/MapChart.js
@@ -70,7 +70,7 @@ const MapChart = ( props ) => {
                                    strokeLinejoin="round"
                                    transform="translate(-12, -24)"
                                 >
-                                    <circle cx="12" cy="10" r="3" className={"marker continent-marker"}/>
+                                    <circle cx="12" cy="10" r="3"/>
                                     <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
                                 </g>
                             </Marker>

5891e4e6eed596ccc46792e9336c2050c58d77f2
bby2k <bakbence.2k@gmail.com>
2022-08-19 08:32:19 +0200

    Feat: Final MapChart.js

--- src/frontend/terra-visio/src/worldmap/MapChart.js
+++ src/frontend/terra-visio/src/worldmap/MapChart.js
@@ -1,4 +1,5 @@
 import React, {memo, useEffect, useState} from "react";
+import {useNavigate} from "react-router-dom";
 import {
     ZoomableGroup,
     ComposableMap,
@@ -6,6 +7,7 @@ import {
     Geography,
     Marker
 } from "react-simple-maps";
+import {toggleVisibilityOnRollback} from "./rollback";
 
 const MapChart = ( props ) => {
     const[continentData, setContinentData] = useState([]);
@@ -17,104 +19,153 @@ const MapChart = ( props ) => {
             .then(data => setContinentData(data))
     }, [])
 
-    return (
-        <div data-tip="" style={{height: "100%", width: "50%", border:"2px solid black", display:"flex", justifyContent:"center"}}>
-            <ComposableMap>
-                <ZoomableGroup center={props.center} zoom={1}>
-                    <Geographies geography="/features.json">
-                        {({ geographies }) =>
-                            geographies.map((geo) => (
-                                <Geography
-                                    class={geo.properties.continent !=null ? "country " + geo.properties.continent : "country undefined-continent"}
-                                    id={geo.properties.name.toLowerCase()}
-                                    key={geo.rsmKey}
-                                    geography={geo}
-                                    onMouseEnter={() => {
-                                        props.setTooltipContent(`${geo.properties.name}`);
-                                    }}
-                                    onMouseLeave={() => {
-                                        props.setTooltipContent("");
-                                    }}
-                                    style={{
-                                        default: {
-                                            // fill: "#D6D6DA",
-                                            outline: "none"
-                                        },
-                                        hover: {
-                                            // fill: "#F53",
-                                            outline: "none"
-                                        },
-                                        pressed: {
-                                            // fill: "#E42",
-                                            outline: "none"
-                                        }
-                                    }}
-                                />
-                            ))
-                        }
-                    </Geographies>
-                    {continentData.length===0?"":continentData.map(data => {
-                        let altitude = data.altitude
-                        let longitude = data.longitude
-                        return(
-                            <Marker coordinates={[altitude, longitude]}>
-                                <g className={"marker continent-marker"}
-                                   fill="#99CD7D"
-                                   stroke="#fff"
-                                   onClick={() => {
-                                       toggleVisibilityOnContinentMarker();
-                                       setHighlight(data.name);
-                                   }}
-                                   strokeWidth="2"
-                                   strokeLinecap="round"
-                                   strokeLinejoin="round"
-                                   transform="translate(-12, -24)"
-                                >
-                                    <circle cx="12" cy="10" r="3"/>
-                                    <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
-                                </g>
-                            </Marker>
-                        )
-                        }
-                    )}
-                    {/*GREAT-CORAL-REEF*/}
-                    {/*<Marker coordinates={[510, -35]}>*/}
+    const[locationData, setLocationData] = useState([]);
+    useEffect(() => {
+        fetch(`/api/locations`,
+            {method:"GET",
+                headers:{"accept":"application/json"}})
+            .then(res => res.json())
+            .then(data => setLocationData(data))
+    }, [])
+    console.log(locationData)
 
-                    {/*    <g className={"marker continent-marker"}*/}
-                    {/*       fill="none"*/}
-                    {/*       stroke="#7dc1cd"*/}
-                    {/*       strokeWidth="2"*/}
-                    {/*       strokeLinecap="round"*/}
-                    {/*       strokeLinejoin="round"*/}
-                    {/*       transform="translate(-12, -24)"*/}
-                    {/*    >*/}
-                    {/*        <circle cx="12" cy="10" r="3"/>*/}
-                    {/*        <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />*/}
-                    {/*    </g>*/}
-                    {/*</Marker>*/}
-                </ZoomableGroup>
-            </ComposableMap>
+    return (
+        <div style={{display: "flex", justifyContent: "center"}}>
+            <div data-tip="" style={{height: "100%", width: "1100px", border:"2px solid black"}}>
+                <ComposableMap>
+                    <ZoomableGroup center={props.center} zoom={1}>
+                        <Geographies geography="/features.json">
+                            {({ geographies }) =>
+                                geographies.map((geo) => (
+                                    <Geography
+                                        class={geo.properties.continent !=null ? "country " + geo.properties.continent : "country undefined-continent"}
+                                        id={geo.properties.name.toLowerCase()}
+                                        key={geo.rsmKey}
+                                        geography={geo}
+                                        onMouseEnter={() => {
+                                            props.setTooltipContent(`${geo.properties.name}`);
+                                        }}
+                                        onMouseLeave={() => {
+                                            props.setTooltipContent("");
+                                        }}
+                                        style={{
+                                            default: {
+                                                // fill: "#D6D6DA",
+                                                outline: "none"
+                                            },
+                                            hover: {
+                                                // fill: "#F53",
+                                                outline: "none"
+                                            },
+                                            pressed: {
+                                                // fill: "#E42",
+                                                outline: "none"
+                                            }
+                                        }}
+                                    />
+                                ))
+                            }
+                        </Geographies>
+                        {continentData.length===0?"":continentData.map(data => {
+                                let altitude = data.altitude
+                                let longitude = data.longitude
+                                return(
+                                    <Marker coordinates={[altitude, longitude]}>
+                                        <g className={"marker continent-marker"}
+                                           fill="#99CD7D"
+                                           stroke="#fff"
+                                           onClick={() => {
+                                               toggleVisibilityOnContinentMarker();
+                                               setHighlight(data.name);
+                                               displayLocations(data.id);
+                                               toggleVisibilityOnRollback()
+                                           }}
+                                           strokeWidth="2"
+                                           strokeLinecap="round"
+                                           strokeLinejoin="round"
+                                           transform="translate(-12, -24)"
+                                        >
+                                            <circle cx="12" cy="10" r="3"/>
+                                            <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                                        </g>
+                                    </Marker>
+                                )
+                            }
+                        )}
+                        {locationData.length===0?"":locationData.map(data => {
+                            let altitude = data.altitude
+                            let longitude = data.longitude
+                            return(
+                                <Marker coordinates={[altitude, longitude]}>
+                                    <g className={"marker location-marker continent-"+data.continentId}
+                                       fill="#99CD7D"
+                                       stroke="#fff"
+                                       display="none"
+                                       onClick={() => {
+                                           // toggleVisibilityOnContinentMarker();
+                                           // setHighlight(data.name);
+                                           NavigateToInfoPage(data.name);
+                                       }}
+                                       strokeWidth="2"
+                                       strokeLinecap="round"
+                                       strokeLinejoin="round"
+                                       transform="translate(-12, -24)"
+                                    >
+                                        <circle cx="12" cy="10" r="3"/>
+                                        <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+                                    </g>
+                                </Marker>
+                            )
+                        })}
+
+                        {/*GREAT-CORAL-REEF*/}
+                        {/*<Marker coordinates={[510, -35]}>*/}
+
+                        {/*    <g className={"marker continent-marker"}*/}
+                        {/*       fill="none"*/}
+                        {/*       stroke="#7dc1cd"*/}
+                        {/*       strokeWidth="2"*/}
+                        {/*       strokeLinecap="round"*/}
+                        {/*       strokeLinejoin="round"*/}
+                        {/*       transform="translate(-12, -24)"*/}
+                        {/*    >*/}
+                        {/*        <circle cx="12" cy="10" r="3"/>*/}
+                        {/*        <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />*/}
+                        {/*    </g>*/}
+                        {/*</Marker>*/}
+                    </ZoomableGroup>
+                </ComposableMap>
+            </div>
         </div>
+
     );
 };
 
-function toggleVisibilityOnContinentMarker(){
+export function toggleVisibilityOnContinentMarker(){
     const continentMarkers = document.getElementsByClassName("continent-marker");
     for (let marker of continentMarkers) {
         marker.style.display==="none"?marker.style.display="block":marker.style.display="none"
     }
 }
+
 function setHighlight(continentName){
     const countriesInContinent = document.getElementsByClassName(continentName.toLowerCase());
     for (let country of countriesInContinent) {
         country.style.fill = "#194952"
     }
 }
-function removeHighlight(){
-    const countriesInContinent = document.getElementsByClassName("country");
-    for (let country of countriesInContinent) {
-        country.style.fill = ""
+
+
+function displayLocations(id) {
+    const locations = document.getElementsByClassName("continent-"+id);
+    for (let location of locations) {
+        location.style.display = "block"
     }
+}
+
+function NavigateToInfoPage(name) {
+    const navigate = useNavigate();
+    navigate("/")
 }
 
 export default memo(MapChart);

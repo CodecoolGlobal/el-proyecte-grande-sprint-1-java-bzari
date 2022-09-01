f3dcc175dfb50b3e3fd974a1ca44b5982643da68
AronBodzas <aron.bodzas42@gmail.com>
2022-09-01 11:49:00 +0200

    Refactor: shell script for jar file

---
+++ build.sh
@@ -0,0 +1,7 @@
+cd backend/
+mvn clean package
+cd ..
+export DB_USERNAME=aron
+export DB_NAME=terraVisio
+export DB_PASSWORD=Sisakvirag17
+java -jar backend/target/el-proyecte-grande-sprint-1-java-bzari-1.0-SNAPSHOT.jar
